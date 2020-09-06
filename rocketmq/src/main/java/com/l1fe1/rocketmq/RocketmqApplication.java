3d887b041abd697c40df10e394bf7c3381da600c
L1fe1 <316772957@qq.com>
2020-09-06 14:03:07 +0800

    添加rocketmq模块

---
+++ rocketmq/src/main/java/com/l1fe1/rocketmq/RocketmqApplication.java
@@ -0,0 +1,13 @@
+package com.l1fe1.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqApplication.class, args);
+	}
+
+}
---
+++ rocketmq/src/test/java/com/l1fe1/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l1fe1.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
