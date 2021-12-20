085c09ec76cd50bfdfb5337a54cb3a58e7241b57
L1fe1 <316772957@qq.com>
2021-12-20 22:37:22 +0800

    feat:新增 spring cloud 模块

---
+++ springcloud/src/test/java/com/l1fe1/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l1fe1.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
