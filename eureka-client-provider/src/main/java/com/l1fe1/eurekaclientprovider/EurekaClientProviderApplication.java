b19cc90cdf2d1c514cdf351d0b3c1aa79d19f492
L1fe1 <316772957@qq.com>
2021-12-26 23:58:05 +0800

    feat:新增 eureka-client-provider

---
+++ eureka-client-provider/src/main/java/com/l1fe1/eurekaclientprovider/EurekaClientProviderApplication.java
@@ -0,0 +1,13 @@
+package com.l1fe1.eurekaclientprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientProviderApplication.class, args);
+	}
+
+}
