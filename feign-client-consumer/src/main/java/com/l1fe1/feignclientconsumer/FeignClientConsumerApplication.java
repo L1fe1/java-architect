2bc862b4eff94654487c11cbd85ca7340fd5e8db
L1fe1 <316772957@qq.com>
2022-01-09 22:59:44 +0800

    feat：新增 hystrix dashboard

--- feign-client-consumer/src/main/java/com/l1fe1/feignclientconsumer/FeignClientConsumerApplication.java
+++ feign-client-consumer/src/main/java/com/l1fe1/feignclientconsumer/FeignClientConsumerApplication.java
@@ -2,10 +2,14 @@ package com.l1fe1.feignclientconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class FeignClientConsumerApplication {
 
 	public static void main(String[] args) {
