173372bca9bbfcf8431c4dd9d3ccd00131a548cc
L1fe1 <316772957@qq.com>
2022-01-30 20:26:08 +0800

    feat:新增 zuul 容错代码

---
+++ zuul-server/src/main/java/com/l1fe1/zuulserver/ZuulFallback.java
@@ -0,0 +1,65 @@
+package com.l1fe1.zuulserver;
+
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@Component
+public class ZuulFallback implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause instanceof HystrixTimeoutException) {
+            return response(HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            return response(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private ClientHttpResponse response(final HttpStatus status) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() {
+                return status;
+            }
+
+            @Override
+            public int getRawStatusCode() {
+                return status.value();
+            }
+
+            @Override
+            public String getStatusText() {
+                return status.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public InputStream getBody() {
+                String msg = "{\"msg\":\"服务故障\"}";
+                return new ByteArrayInputStream(msg.getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
