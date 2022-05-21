bed5edd72e12e9ea97da0790303d236f57c25131
L1fe1 <316772957@qq.com>
2022-05-21 22:36:34 +0800

    新增 data jpa、netty 内容

---
+++ netty/src/test/java/com/l1fe1/netty/NettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l1fe1.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
