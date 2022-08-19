59736516426c108eb10d65f6e49eb1cb0551767c
fei4 <fei4@michaels.com>
2022-08-19 17:38:25 +0800

    feat:add demo main

---
+++ skywalking/demo-main/src/main/java/com/l1fe1/Main.java
@@ -0,0 +1,8 @@
+package com.l1fe1;
+
+public class Main {
+    public static void main( String[] args ) throws Exception {
+        Thread.sleep(1000);
+        System.out.println("DemoMain Main!");
+    }
+}
